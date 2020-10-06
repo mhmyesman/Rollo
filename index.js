9d45bd3a13ea5565b00cf7086cf6048a90c6829b
mhmyesman <mhmyesman@users.noreply.github.com>
2020-10-06 11:03:03 -0400

    fix bad module name

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const Discord = require('discord.');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    console.log("Ready!");
-})
+	console.log('Ready!');
+});
 
-client.login('bot-token')
+client.login('bot-token');
